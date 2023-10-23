0851f50795ec1bfb67bc659298c1ff0fd77b331f
Roy <vargas.sap321@gmail.com>
2023-10-23 02:19:23 -0500

    fix: identation error

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -9,8 +9,7 @@ request(URL, (error, response, body) => {
   const todos = {};
   data.forEach(task => {
     if (task.completed) {
-      if (todos[task.userId])
-        todos[task.userId]++;
+      if (todos[task.userId]) todos[task.userId]++;
       else todos[task.userId] = 1;
     }
   });
