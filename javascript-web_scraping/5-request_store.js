0f347dce101616a0a9a929363f8e7d9b53aecb8a
Roy <vargas.sap321@gmail.com>
2023-10-23 00:49:52 -0500

    feat: script that gets the contents of a webpage and stores it in a file

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const URL = process.argv[2];
+const path = process.argv[3];
+const request = require('request');
+const fs = require('fs');
+
+request(URL, (error, response, body) => {
+  if (error) console.error(error);
+  else {
+    fs.writeFile(path, body, (err) => {
+      if (err) console.error(err);
+    });
+  }
+})
