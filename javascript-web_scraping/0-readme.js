012a525d6d107c033c18706567e1497f62b62d2c
Roy <vargas.sap321@gmail.com>
2023-10-22 20:24:00 -0500

    feat: script that reads and prints the content of a file

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const process = require('process');
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf-8', (error, data) => {
+    if (error) console.error(error);
+    else console.log(data);
+});
