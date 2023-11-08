b5e4c08a4c755f4edb2e207e969b84614cbe1d89
Roy <vargas.sap321@gmail.com>
2023-11-08 12:58:25 -0500

    fix: lint error

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,5 +1,5 @@
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', data => {
+process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data.toString()}`);
   console.log('This important software is now closing');
   process.stdin.end();
