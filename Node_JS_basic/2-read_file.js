a586677ec3503ed02669690a85e8fbc8a4f31a47
Roy <vargas.sap321@gmail.com>
2023-11-09 12:13:28 -0500

    fix: lint error

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -2,30 +2,31 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, 'utf8');
-    const rows = data.split('\n');
-    const students = {};
-
-    for (const row of rows) {
-      if (row.trim() !== '') {
-        const [firstName, , , field] = row.split(',');
-
-        if (firstName) {
-          if (students[field]) {
-            students[field].push(firstName);
-          } else {
-            students[field] = [firstName];
-          }
-        }
+    const allLines = fs.readFileSync(path, 'utf-8').trim().split('\r\n');
+    const dataLines = allLines.slice(1)
+    const headers = allLines[0].split(',');
+    const result = [];
+    dataLines.map((line) => {
+      const obj = {}
+      l = line.split(',');
+      headers.map((h, i) => {
+        obj[h] = l[i];
+      });
+      result.push(obj);
+    })
+    const nameCS = [];
+    const nameSWE = [];
+    result.forEach(line => {
+      if (line['field'] === 'CS'){
+        nameCS.push(line['firstname']);
       }
-    }
-
-    const totalStudents = (students['CS\r'].length + students['SWE\r'].length);
-    console.log(`Number of students: ${totalStudents}`);
-    const studentsSWE = students['SWE\r'].join(', ');
-    const studentsCS = students['CS\r'].join(', ');
-    console.log(`Number of students in CS: ${students['CS\r'].length}. List: ${studentsCS}`);
-    console.log(`Number of students in SWE: ${students['SWE\r'].length}. List: ${studentsSWE}`);
+      if (line['field'] === 'SWE'){
+        nameSWE.push(line['firstname']);
+      }
+    });
+    console.log(`Number of students: ${result.length}`);
+    console.log(`Number of students in CS: ${nameCS.length}. List: ${nameCS.join(', ')}`);
+    console.log(`Number of students in SWE: ${nameSWE.length}. List: ${nameSWE.join(', ')}`);
   } catch (e) {
     throw new Error('Cannot load the database');
   }
