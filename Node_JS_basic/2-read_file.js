ae867772fa961856f0c91128f73582e43d8ac9c3
Roy <vargas.sap321@gmail.com>
2023-11-09 01:13:27 -0500

    feat: Reading a file synchronously with Node JS

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -8,7 +8,7 @@ function countStudents(path) {
 
     for (const row of rows) {
       if (row.trim() === '') {
-        /*eslint no-continue: "error"*/
+        /* eslint no-continue: "error" */
         continue;
       }
       const [firstName, , , field] = row.split(',');
