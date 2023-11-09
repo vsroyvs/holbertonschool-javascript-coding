14d4b6c490c053a67c03129a2fc2bbdd47a51655
Roy <vargas.sap321@gmail.com>
2023-11-09 00:32:00 -0500

    feat: Reading a file synchronously with Node JS

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const rows = data.split('\n');
+    const students = {};
+
+    for (const row of rows) {
+      if (row.trim() === '') continue;
+
+      const [firstName, , , field] = row.split(',');
+
+      if (firstName) {
+        if (students[field]) {
+          students[field].push(firstName);
+        } else {
+          students[field] = [firstName];
+        }
+      }
+    }
+
+    let totalStudents = (students['CS\r'].length + students['SWE\r'].length)
+    console.log(`Number of students: ${totalStudents}`);
+    const studentsSWE = students['SWE\r'].join(', ');
+    const studentsCS = students['CS\r'].join(', ');
+    console.log(`Number of students in CS: ${students['CS\r'].length}. List: ${studentsCS}`);
+    console.log(`Number of students in SWE: ${students['SWE\r'].length}. List: ${studentsSWE}`);
+  } catch (e) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
